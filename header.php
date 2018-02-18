6fd0c3b01c737624180f1281d70c95fe09585046
RGomes97 <rubensgagostinho@outlook.com>
2018-02-18 14:37:07 -0300

    Inclusao de meta viewport

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 <html>
 <head>
 	<title> Gráfica Flaguris</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
